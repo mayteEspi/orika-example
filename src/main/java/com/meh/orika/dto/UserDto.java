27e4621f6b9eedc4e8c9dc50cb6fe0ac1e952035
Mayte Espi <maytehermida@gmail.com>
2018-07-15 21:55:07 +0200

    add lombok & mapper

---
+++ src/main/java/com/meh/orika/dto/UserDto.java
@@ -0,0 +1,10 @@
+package com.meh.orika.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDto {
+
+}
---
+++ src/main/java/com/meh/orika/mapper/Mapper.java
@@ -0,0 +1,10 @@
+package com.meh.orika.mapper;
+
+import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.impl.ConfigurableMapper;
+
+public class Mapper extends ConfigurableMapper{
+	
+	protected void configure(MapperFactory factory) {
+	}
+}
